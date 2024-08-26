97f3545f51c124cf84b2758c64ebcf8582b2462f
Muhammad Haekal <49970186+hcalldee@users.noreply.github.com>
2024-08-26 09:35:21 +0800

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    transform: {
-      '^.+\\.mjs$': 'babel-jest',
-      '^.+\\.js$': 'babel-jest', // Add if you have .js files
-    },
-    testMatch: ['**/unitTest/**/*.test.js'],
-    moduleFileExtensions: ['js', 'mjs'],
-    testEnvironment: 'node',
-  };
